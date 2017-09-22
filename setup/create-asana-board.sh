5d163da9af61cd10201c2399605f7782d026cdee
Stephen Taylor Witte <stevetwitte@gmail.com>
2017-09-22 13:17:58 -0400

    makes team and workspace variables

--- setup/create-asana-board.sh
+++ setup/create-asana-board.sh
@@ -4,6 +4,8 @@
 # Must add the Asana access token that can be generated from your profile settings
 
 access_token=
+workspace_id=285734793289376
+team_id=285734793289378
 
 echo "Board name?"
 read board_name
@@ -14,7 +16,7 @@ if [ "$board_name" = "" ]; then
 fi
 
 echo -n "Creating Asana board named '$board_name' ..."
-project_output=$(curl -H "Authorization: Bearer $access_token" https://app.asana.com/api/1.0/projects --data-urlencode "name=$board_name" --data-urlencode "workspace=285734793289376" --data-urlencode "team=285734793289378" --data-urlencode "layout=board")
+project_output=$(curl -H "Authorization: Bearer $access_token" https://app.asana.com/api/1.0/projects --data-urlencode "name=$board_name" --data-urlencode "workspace=$workspace_id" --data-urlencode "team=$team_id" --data-urlencode "layout=board")
 echo " done."
 
 echo "$project_output"
@@ -28,10 +30,10 @@ if [ "$project_id" = "" ]; then
 fi
 
 echo -n "Creating board columns ..."
-curl -H "Authorization: Bearer 0/cdbd95487a460985f33d3a56dd7e6bf8" https://app.asana.com/api/1.0/projects/"$project_id"/sections --data-urlencode "name=Reference" > /dev/null 2>&1
-curl -H "Authorization: Bearer 0/cdbd95487a460985f33d3a56dd7e6bf8" https://app.asana.com/api/1.0/projects/"$project_id"/sections --data-urlencode "name=Backlog" > /dev/null 2>&1
-curl -H "Authorization: Bearer 0/cdbd95487a460985f33d3a56dd7e6bf8" https://app.asana.com/api/1.0/projects/"$project_id"/sections --data-urlencode "name=Next Sprint" > /dev/null 2>&1
-curl -H "Authorization: Bearer 0/cdbd95487a460985f33d3a56dd7e6bf8" https://app.asana.com/api/1.0/projects/"$project_id"/sections --data-urlencode "name=This Sprint" > /dev/null 2>&1
-curl -H "Authorization: Bearer 0/cdbd95487a460985f33d3a56dd7e6bf8" https://app.asana.com/api/1.0/projects/"$project_id"/sections --data-urlencode "name=QA" > /dev/null 2>&1
-curl -H "Authorization: Bearer 0/cdbd95487a460985f33d3a56dd7e6bf8" https://app.asana.com/api/1.0/projects/"$project_id"/sections --data-urlencode "name=Done" > /dev/null 2>&1
+curl -H "Authorization: Bearer $access_token" https://app.asana.com/api/1.0/projects/"$project_id"/sections --data-urlencode "name=Reference" > /dev/null 2>&1
+curl -H "Authorization: Bearer $access_token" https://app.asana.com/api/1.0/projects/"$project_id"/sections --data-urlencode "name=Backlog" > /dev/null 2>&1
+curl -H "Authorization: Bearer $access_token" https://app.asana.com/api/1.0/projects/"$project_id"/sections --data-urlencode "name=Next Sprint" > /dev/null 2>&1
+curl -H "Authorization: Bearer $access_token" https://app.asana.com/api/1.0/projects/"$project_id"/sections --data-urlencode "name=This Sprint" > /dev/null 2>&1
+curl -H "Authorization: Bearer $access_token" https://app.asana.com/api/1.0/projects/"$project_id"/sections --data-urlencode "name=QA" > /dev/null 2>&1
+curl -H "Authorization: Bearer $access_token" https://app.asana.com/api/1.0/projects/"$project_id"/sections --data-urlencode "name=Done" > /dev/null 2>&1
 echo " done."
